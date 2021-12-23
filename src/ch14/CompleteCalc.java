a7999a0d39276cd253ff8a7ca348b9922e18697a
Dyaos <eogus12003@naver.com>
2021-12-23 20:27:14 +0900

    21.12.23

--- src/ch14/CompleteCalc.java
+++ src/ch14/CompleteCalc.java
@@ -1,6 +1,6 @@
-package ch11;
+package ch14;
 
-public class CompleteCalc extends Calculator{
+public class CompleteCalc extends Calculator {
     @Override
     public int times(int num1, int num2) {
         return num1 * num2;
